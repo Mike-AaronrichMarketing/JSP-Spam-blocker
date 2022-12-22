055c0a348b918966f271cfa309c9ba9934193bef
Mike-AaronrichMarketing <109161216+Mike-AaronrichMarketing@users.noreply.github.com>
2022-12-22 09:53:57 -0600

    Create fightbackspam.js

---
+++ fightbackspam.js
@@ -0,0 +1,38 @@
+
+
+<script>
+// We are allowing 0 urls or regular expressions that can be changed from this site. https://uibakery.io/regex-library/url
+
+
+        var allowed = 0; //allowed times
+        var regex = /(http|https|ftp|www):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i; //match url
+
+        $('#message').on('input', function () { // match the ID to the field on the form being used
+            var textArea = $(this).val().match(regex); // search string
+            if (textArea && textArea.length > allowed) { // capture the textarea and the length of the expression
+                $('#submit').prop("disabled", true); // disable submit button 
+            } else {
+                $('#submit').prop("disabled", false); // if no regular expression matches it will not disable the submit button 
+            }
+        });
+    </script>
+    <script>
+    
+    // This function targets the amount of characters on that are allowable on the contact form. 
+    
+        $('#message').keypress(function () {
+            var maxLength = $(this).val().length;
+            if (maxLength >= 200) {
+                alert('You cannot enter more than ' + maxLength + ' chars');
+                return false;
+            }
+        });
+    </script>
+
+	<script>
+  // This will prevent the option to copy and paste into the field ID
+  
+        $('#message').bind('copy paste', function (e) {
+            e.preventDefault();
+        });
+    </script>
